ff41f342e992d72c80168b8df38e77e9438452d5
Andrew Mason <andrewmason@Andrews-MacBook-Pro.local>
2021-04-22 15:31:20 -0400

    Update to welcome page buttons, body max-width for better card title spacing

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -48,13 +48,14 @@
             class="w-75 h-100" 
             style="object-fit: cover; position: absolute; z-index: 0; right: 0; top: 0;"></b-img>
             <b-col xl="8" lg="9"
-            style="position: relative; z-index: 3"
+            style="position: relative; z-index: 3;"
             class="d-flex flex-column justify-content-center align-items-start p-5 mt-5 mt-md-0">
                 <div class="shadow p-5" style="background: rgba( 255, 255, 255, 0.9 );
                     box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
                     backdrop-filter: blur( 4px );
                     -webkit-backdrop-filter: blur( 4px );
-                    border: 1px solid rgba( 255, 255, 255, 0.18 );">
+                    border: 1px solid rgba( 255, 255, 255, 0.18 );
+                    border-radius: 14px">
                     <hr class="d-flex m-0 mr-auto bg-warning" style="height: 4px; width: 50px;">
                     <h5 class="py-3 mt-3 mb-0 font-weight-bold text-muted" v-html="items.microTitle"></h5>
                     <h1 class="display-2 font-weight-bold mb-4"
@@ -71,15 +72,15 @@
                             <div class="d-flex mb-4">
                                 <router-link to="/home">
                                     <b-button 
-                                    variant="dark" 
+                                    variant="coral" 
                                     size="lg"
-                                    class="rounded-0 shadow mr-2" v-html="items.btn1"></b-button>
+                                    class="shadow mr-2" v-html="items.btn1"></b-button>
                                 </router-link>
                                 <router-link to="/about">
                                     <b-button 
-                                    variant="outline-dark" 
+                                    variant="lightblue" 
                                     size="lg"
-                                    class="rounded-0 shadow mr-2" v-html="items.btn2"></b-button>
+                                    class="shadow mr-2" v-html="items.btn2"></b-button>
                                 </router-link>
                             </div>
                             </div>
