3d8fd922ca987b50c3cbed96a5c17a04cdef1ca6
Andrew Mason <andrewmason@Andrews-MacBook-Pro.local>
2021-05-10 15:07:25 -0400

    Converted menu to JSON with v-for in headercomponent

--- src/components/common/HeaderComponent.vue
+++ src/components/common/HeaderComponent.vue
@@ -1,39 +1,18 @@
 <template>
-
-<!-- <div class="w-100 bg-light py-2 px-3 d-flex justify-content-end align-items-center" style="position: absolute; z-index: 100; right: 0; top: 0;">
-    <svg v-b-toggle.sidebar-right xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-filter-right" viewBox="0 0 16 16">
-        <path d="M14 10.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0 0 1h7a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0 0 1h11a.5.5 0 0 0 .5-.5z"/>
-    </svg>
-
-    <b-sidebar id="sidebar-right" title="Sidebar" right shadow>
-      <div class="px-4 py-2 d-flex align-self-end display-3 text-right">
-        <b-navbar-nav class="ml-auto">
-          <router-link class="nav-link" to="/home">Home</router-link>
-          <router-link class="nav-link" to="/about">About</router-link>
-          <router-link class="nav-link" to="/food-details">Details</router-link>
-          <router-link class="nav-link" to="/cart">My Plan</router-link>
-          <router-link class="nav-link" to="/cart">
-          <b-badge class="cart-count" variant="primary">{{ count }}</b-badge>
-            <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
-              <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-            </svg>
-            <span class="cart-count">{{ count }}</span>
-          </router-link>
-      </b-navbar-nav>
-      </div>
-    </b-sidebar>
-  </div> -->
   
+  <div>
   <b-navbar 
   fixed="top" 
   toggleable="lg" 
   type="light" 
   variant="light" 
-  class="shadow py-3 py-lg-2">
-
-    <router-link class="navbar-brand font-weight-bold d-flex align-items-center" to="/">
-    Leadership Learning Tool
+  class="shadow py-3 py-lg-2"
+  v-for="items in $t('menu')" :key="items.id">
+    <router-link 
+    class="d-flex navbar-brand font-weight-bold align-items-center" to="/"
+    v-html="items.navbarBrand">
     </router-link> 
+
     <b-navbar-toggle target="nav-collapse">
       <!-- custom mobile toggle icon -->
       <template #default="{ expanded }">
@@ -88,12 +67,12 @@
     </b-sidebar> -->
 
     <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav class="ml-auto d-flex justify-content-center align-items-start align-items-lg-center">
-          <router-link class="nav-link" to="/">Home</router-link>
-          <router-link class="nav-link" to="/get-started">Get Started</router-link>
-          <router-link class="nav-link" to="/about">About This Tool</router-link>
-          <router-link class="nav-link" to="/my-plan">My Plan</router-link>
-          
+      <b-navbar-nav 
+      class="d-flex justify-content-center align-items-start align-items-lg-center ml-auto">
+        <span v-for="item in items.menuItems" :key="item.id">
+            <router-link class="nav-link" :to="item.url" v-html="item.link"></router-link>
+        </span>
+
           <!-- //sidebar activate  -->
           <!-- <div v-b-toggle.sidebar-right class="nav-link mr-3">
             <user-icon class="user-icon"></user-icon>
@@ -119,6 +98,7 @@
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
+  </div>
 </template>
 
 <script>
@@ -160,6 +140,9 @@ export default {
                 price += el["quantity"] * el["id"]
             })
             return price;
+        },
+        menu() {
+          return this.$store.state.data.menu;
         },
       count() {
         return this.$store.state.cartItemCount;
